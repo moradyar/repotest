7ca2d2b07655813f55ba0b973a9310ecca579ef0
Mohammad Moradyar <moradyar.mohammad@gmail.com>
2021-10-25 15:15:00 +0200

    Second

--- app/src/main/java/com/moradyar/repotest/MainActivity.kt
+++ app/src/main/java/com/moradyar/repotest/MainActivity.kt
@@ -9,9 +9,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         CoroutineScope(Dispatchers.IO).launch {
             val result = RickAndMortyApi().getEpisodeList()
             Log.i("XXXX", result)
